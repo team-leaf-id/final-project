2b38726ec5d5f105ca6ccec7d7230641584079f8
joannaarroyo <joliuac@gmail.com>
2019-06-26 12:28:39 -0700

    started sustainability tests in server.js

--- server.js
+++ server.js
@@ -95,92 +95,28 @@ function searchFish(request, response){
     .catch(error => handleError(error, response));
 }
 
-// function searchFish(request, response){
-//   // const url = `https://www.fishwatch.gov/api/species`;
-//   let searchQuery = request.body.search.toLowerCase();
-//   const SQL = `SELECT path FROM fish WHERE species_name LIKE '%${searchQuery}%' OR species_aliases LIKE '${searchQuery}';`;
-//   return client.query(SQL)
-//     .then(results => {
-//       let fishArray = [];
-//       results.rows.forEach(row => {
-//         getFishDetails(row.path)
-//           .then(fishfish => {
-//             console.log('FISH', fishfish);
-//             fishArray.push(fishfish);
-//           });
-//       })
-//       return fishArray;
-//     })
-//     .then(results => response.render('searches/show', {results: results}))
-//     .catch(error => handleError(error, response));
-// }
+function sustainabilityCheck(fishInfo){
+  const sustainableTalk = ['smart seafood choice', 'sustainably managed']; //Can this be an array?
+  if(fishInfo.quote.includes(sustainableTalk)){
+    let marker = 'canSustain.jpg'; //Get image from Yoshi
+    let text = 'This fish is sustainable and a smart seafood choice! Here are some recipes:';
+    let recipes = findRecipes(fishInfo);
+    return marker, text, recipes;
+  } else {
+    let marker = 'notSustain.jpg';
+    let text = 'This fish is not a smart seafood choice. Here are other fish that you may enjoy:';
+    let alt = findAlt(fishInfo);
+    return marker, text, alt;
+  }
+}
 
-// function getFishDetails(path){
-//   const url = `https://www.fishwatch.gov/api/species${path}`;
-//   return superagent.get(url)
-//     .then(results => {
-//       let fishfish = new Fish(results.body[0]);
-//       // console.log('GET FISH URL RESULTS', fishfish);
-//       return fishfish;
-//     })
-// }
+function findRecipes(fishInfo){ //Which recipe API to use?
 
-// function getLocation(query, client, superagent) {
-//   return getStoredLocation(query, client).then(location => {
-//     if (location) {
-//       return location;
-//     } else {
-//       return getLocationFromApi(query, client, superagent);
-//     }
-//   });
-// }
+}
 
-// function getStoredLocation(query, client) {
-//   const sql = `SELECT * FROM locations WHERE search_query='${query}'`;
-
-//   return client.query(sql).then(results => results.rows[0]);
-// }
-
-// function createSearch(request, response) {
-//   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-
-//   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
-//   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
-
-//   superagent.get(url)
-//     .then(apiResponse => apiResponse.body.items.map(bookResult =>{
-
-//       let bookArr = new Book(bookResult.volumeInfo);
-//       console.log(bookArr);
-//       return bookArr;
-//     })
-    
-//     )
-//     .then(results => response.render('pages/searches/show', { results: results }))
-//     .catch(err => handleError(err, response));
-// }
-
-
-// function getFish(request, response){
-//   let url = `https://www.fishwatch.gov/api/species`;
-
-//   // console.log('request: ', request.body);
-//   // console.log('URL: ', url);
-
-//   superagent.get(url)
-//     // .then(apiResponse => {
-//     //   console.log('RESPONSE BODY', apiResponse.body);
-//     //   apiResponse.body.map(plantResult => {
-//     //     // console.log('RESPONSE', apiResponse);
-//     //     let plantArray = new Plant(plantResult);
-//     //     return plantArray;
-//     //   })})
-//     .then(results => {
-//       console.log('RESPONSE BODY>>>>>>>>>>>>', results.body);
-//       response.render('searches/show', {results: results.body})})
-//     .catch(error => handleError(error, response));
-
-// }
+function findAlt(fishInfo){ 
+  
+}
 
 // Constructor Function
 function Fish(result){
