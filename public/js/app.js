8cc6fd640d5e676089813c64b4dd5389f8ee5378
lilbonito <sauteboy@gmail.com>
2019-06-28 10:26:59 -0700

    fish

--- public/js/app.js
+++ public/js/app.js
@@ -5,4 +5,27 @@ $(document).ready(function() {
   $('.top-bar').click(function() {
     $('#logo').toggle(1, 'linear');
   });
+
+  $('.logo-box').click(function() {
+    $('.search-box').toggle('blind', { direction: 'vertical' }, 'fast');
+  });
+  $('.logo-box').click(function() {
+    $('#logo').toggle(1, 'linear');
+  });
+
+  $('.set > a').on('click', function() {
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+      $(this)
+        .siblings('.content')
+        .slideUp(200);
+    } else {
+      $('.set > a').removeClass('active');
+      $(this).addClass('active');
+      $('.content').slideUp(200);
+      $(this)
+        .siblings('.content')
+        .slideDown(200);
+    }
+  });
 });
